001562a6a8c439834b05c15fc0b3e66e98b37c03
faizan <faizansohail077@gmail.com>
2025-02-10 00:01:14 +0500

    added prisma to project

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,13 +1,18 @@
 import express, { Express, Request, Response } from "express";
 import dotenv from "dotenv";
+import { PrismaClient } from '@prisma/client'
+
+const prisma = new PrismaClient()
 
 dotenv.config();
 
 const app: Express = express();
 const port = process.env.PORT || 3000;
 
-app.get("/", (req: Request, res: Response) => {
-  res.send("Express + TypeScript Server");
+app.get("/", async (req: Request, res: Response) => {
+  const user = await prisma.user.findMany()
+  res.send({ message: "Express + TypeScript Server edote" ,user});
+
 });
 
 app.listen(port, () => {
